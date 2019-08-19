7fd3cf6014b2082d500d42042038a80f0d33c392
Matthew Butner <mfbutner@ucdavis.edu>
2019-08-19 14:39:22 -0700

    Changed hello_world.cpp to match what was in the book description.

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,5 +1,10 @@
-#include <iostream>
-int main(){
-    std::cout << "Hello World!" << std::endl;
-    return 0;
+/*
+Here is everyone's favorite teaching program, Hello World,
+in the glorious langauge that is C++.
+*/
+#include <iostream> //iostream is the library for doing input and output
+
+int main(){ // all programs begin at main
+    std::cout << "Hello World!" << std::endl; // the actual printing
+    return 0; //end main and return 0, which stands for program success
 }
